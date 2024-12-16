1cdee72d2d9ae722f21d4c24104285dd09e5c375
Pranjal Gupta <109664273+72pranjal@users.noreply.github.com>
2024-12-16 12:26:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let options = document.querySelector('#colorSelect')
+let button  = document.querySelector('input')
+console.log(button)
+
+button.addEventListener('click', (e)=> {
+    options.options[options.selectedIndex].remove()
+})
